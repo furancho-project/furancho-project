a46788086f058bc55d288184f6720eed7fa2646b
marialalin <mariavencesantorum@gmail.com>
2022-08-02 16:24:29 +0200

    updated footer icon

--- models/furancho.model.js
+++ models/furancho.model.js
@@ -37,7 +37,7 @@ const furanchoSchema = new Schema({
         type: {
             type: String,
             enum: ["Point"],
-            required: true
+            required: true //dame erro
         },
         coordinates: {
             type: [Number],
