152a10887dd2578d41a647bd0605e5277eaaaa1b
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-08-03 19:34:01 +0200

    GMap completed

--- public/js/g-places.js
+++ public/js/g-places.js
@@ -1,5 +1,34 @@
 function initGooglePlaces() {
-    console.log("Places loaded")
+    console.log("Places loaded");
+    initPlacesSearchBar();
+    initGMap();
+}
+
+function initGMap() {
+    const mapContainer = document.querySelector(".g-map");
+    if (mapContainer) {
+        const center = { lat: 42.4336, lng: -8.64805 }
+        const map = new google.maps.Map(mapContainer, {
+            zoom: 9,
+            center: center,
+        });
+
+        if (gMarkers) {
+            gMarkers.forEach(({ title, lng, lat }) => {
+                const image = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+                const marker = new google.maps.Marker({
+                    position: { lng, lat },
+                    map: map,
+                    title,
+                    icon: image
+                });
+
+            });
+        }
+    }
+}
+
+function initPlacesSearchBar() {
 
     const input = document.querySelector(".g-places-finder");
     if (input) {
@@ -15,9 +44,9 @@ function initGooglePlaces() {
             const lat = place.geometry.location.lat();
             const lng = place.geometry.location.lng();
             console.log(place, lat, lng)
-    
+
             document.querySelector("[name='lat']").value = lat;
             document.querySelector("[name='lng']").value = lng;
         });
-    }    
+    }
 }
